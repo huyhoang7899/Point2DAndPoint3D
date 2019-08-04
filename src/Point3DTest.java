00e3ced606a7e7ac5bbfc1e1e05f11579ba7e371
Hoang Minh Huy <hoanghuy070899@gmail.com>
2019-08-04 14:07:14 +0700

    passed

---
+++ src/Point3DTest.java
@@ -0,0 +1,8 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        System.out.println(point3D.toString());
+        point3D = new Point3D(1, 5, 1);
+        System.out.println(point3D.toString());
+    }
+}
